63ba46854e1868de549b6161ffd83925fc494d60
aminbnd <2212@holbertonschool.com>
2020-09-17 09:55:09 +0200

    std error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,11 @@
 /**
 * main - Entry point
 *
-* Return: 0
+* Return: 1
 *
 */
 int main(void)
 {
-write("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
