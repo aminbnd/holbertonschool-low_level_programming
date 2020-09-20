b00e5bb4c012e5fce22818a2bcc12bf00a50a1d1
aminbnd <2212@holbertonschool.com>
2020-09-20 15:24:56 +0200

    combo & elim

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,7 +7,6 @@ int main(void)
 {
 	int a;
 	int b;
-	int c;
 
 	for (a = 0; a < 10; a++)
 	{
@@ -15,8 +14,7 @@ int main(void)
 		{
 			putchar ('0' + a);
 			putchar ('0' + b);
-			c = a * 10 + b;
-			if (c < 89)
+			if (a != 8)
 			{
 				putchar (',');
 				putchar (' ');
