7d0ca3fe43ba64f931df2a80d6890c77aad0a700
aminbnd <2212@holbertonschool.com>
2020-09-24 01:05:37 +0200

    9X

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,7 +13,7 @@ void times_table(void)
 		_putchar('0');
 		_putchar(',');
 
-		for (j = 0; j < 10; j++)
+		for (j = 1; j < 10; j++)
 		{
 			k = j * i;
 			l = k / 10;
@@ -23,7 +23,7 @@ void times_table(void)
 			{
 				_putchar(' ');
 				_putchar(' ');
-				_putchar('0' + k);
+				_putchar('0' + m);
 			}
 			else
 			{
