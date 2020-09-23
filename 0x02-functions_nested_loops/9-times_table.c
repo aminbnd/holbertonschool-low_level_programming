698a6328ad85709a4b99de6b421196bcdf129a58
aminbnd <2212@holbertonschool.com>
2020-09-24 00:57:05 +0200

    9x

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,41 @@
+#include "holberton.h"
+/**
+  *times_table - Prints the x times table
+  * return: void
+  */
+
+void times_table(void)
+{
+	int i, j, k, l, m;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar('0');
+		_putchar(',');
+
+		for (j = 0; j < 10; j++)
+		{
+			k = j * i;
+			l = k / 10;
+			m = k % 10;
+
+			if (k < 10)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + k);
+			}
+			else
+			{
+				_putchar(' ');
+				_putchar('0' + l);
+				_putchar('0' + m);
+			}
+			if (j < 9)
+			{
+				_putchar(',');
+			}
+		}
+		_putchar('\n');
+	}
+}
