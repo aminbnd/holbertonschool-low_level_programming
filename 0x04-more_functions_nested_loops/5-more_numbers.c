a82bfee6ca947ef161a1c452898ef766b8e2dceb
aminbnd <2212@holbertonschool.com>
2020-09-24 23:51:50 +0200

    more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,16 +9,13 @@ void more_numbers(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j < 10; j++)
+		for (j = 0; j < 15; j++)
 		{
-			_putchar('0' + j);
-		}
-		for (j = 10; j <15; j++)
-		{
+			if (j > 9)
 			_putchar('0' + (j / 10));
 			_putchar('0' + (j % 10));
 		}
-		_putchar('\n');
+	_putchar('\n');
 	}
 }
 
