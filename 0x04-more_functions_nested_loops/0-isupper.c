4eca4c09dd0918fd1879f34997cd054870414d3f
aminbnd <2212@holbertonschool.com>
2020-09-24 10:59:12 +0200

    uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+  *_isupper - checks for uppercase character.
+  *Return: 1 if c is uppercase 0 otherwise
+  */
+
+int _isupper(int c)
+{
+
+	if (c > 64 && c < 91)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+		
+
