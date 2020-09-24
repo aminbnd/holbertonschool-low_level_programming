490bce9273b995adbd7d59a39467ee6f4feb9020
aminbnd <2212@holbertonschool.com>
2020-09-24 11:09:21 +0200

    checks number

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+  *_isdigit - checks for a digit
+  *Return: 1 if digit 0 otherwise
+  */
+
+int _isdigit(int c)
+{
+	if (c > 47 && c < 58)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
