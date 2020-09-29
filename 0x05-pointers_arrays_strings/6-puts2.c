92957836f6b9c0cc53221ece0dda07a8d173e50d
aminbnd <2212@holbertonschool.com>
2020-09-29 22:48:41 +0200

    even numbers string

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,11 +9,12 @@ void puts2(char *str)
 	int i = 0;
 	int j;
 
+	i = 0;
 	while (str[i] != '\0')
 	{
 		i++;
 	}
-	for (j = 0; i < j; i = i + 2)
+	for (j = 0; j < i; j = j + 2)
 	{
 		_putchar(str[j]);
 	}
