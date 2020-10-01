c5cdc351bc5d9f91392dd161b0f90994905e25a5
aminbnd <2212@holbertonschool.com>
2020-10-01 20:52:09 +0200

    reverse

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -8,13 +8,13 @@
 void reverse_array(int *a, int n)
 {
 	int i, j;
-	
+
 	i = 0;
 	while (i < n / 2)
 	{
 		j = a[i];
-		a[i] = a[n - i -1];
-		a[n - i -1] = j;
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = j;
 		i++;
 	}
 }
