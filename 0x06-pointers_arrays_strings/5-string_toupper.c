aec337ae74319688d0cebe5de2d24474c668df04
aminbnd <2212@holbertonschool.com>
2020-10-01 21:20:39 +0200

    string to upper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+  *string_toupper - changes lowercase to uppercase
+  *@a: pointer
+  *Return: array of string
+  */
+char *string_toupper(char *a)
+{
+	int i = 0;
+	int j;
+
+	while (a[i] != '\0')
+	{
+		i++;
+	}
+	for (j = 0; a[j] != '\0'; j++)
+	{
+		if (a[j] >= 'a' && a[j] <= 'z')
+		{
+			a[j] -= 32;
+		}
+	}
+	return (a);
+}
