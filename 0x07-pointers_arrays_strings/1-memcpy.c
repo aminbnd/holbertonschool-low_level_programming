5398e807985fdb240f12eb07bec89e956ddaae20
aminbnd <2212@holbertonschool.com>
2020-10-05 08:03:47 +0200

    memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ *_memcpy - copies a memory area
+ *@dest: pointer
+ *@src: pointer
+ *@n: unsigned integer
+ *Return: a pointer to destination
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int i, j;
+for (i = 0; i < n; i++)
+{
+dest[i] = src[j];
+for (j = 0; j < n; j++)
+{
+dest[j] = src[j];
+}
+}
+return (dest);
+}
