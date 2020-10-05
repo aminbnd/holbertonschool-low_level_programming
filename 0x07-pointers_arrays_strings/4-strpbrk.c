24220fe416e428891f40335deb1cab863f83b53d
aminbnd <2212@holbertonschool.com>
2020-10-05 12:29:22 +0200

    strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * _strpbrk - searches a string for any of a set of bytes
+ *@s: pointer
+ *@accept: pointer
+ *Return: pointer to the byte in s that matches one of the bytes in accept
+ */
+char *_strpbrk(char *s, char *accept)
+{
+        int i;
+
+
+
+        return(&s[i]);
+}
