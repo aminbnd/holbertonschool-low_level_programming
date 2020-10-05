b956fe4963e697d14c9750204ca21bb66239bb5e
aminbnd <2212@holbertonschool.com>
2020-10-05 20:32:52 +0200

    strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -2,20 +2,20 @@
 /**
  * _strstr -  locates a substring
  * @haystack: pointer to the string to be "scanned"
- * @needle: substring to be found 
+ * @needle: substring to be found
  * Return: pointer
  */
 char *_strstr(char *haystack, char *needle)
 {
-        int i, j;
+	int i, j;
 
-        for (i = 0; haystack[i]; i++)
-        {
-                j = 0;
-                while (needle[j] && haystack[i + j] == needle[j])
-                j++;
-                if (!needle[j])
-                return (&haystack[i]);
-        }
-        return (0);
+	for (i = 0; haystack[i]; i++)
+	{
+		j = 0;
+		while (needle[j] && haystack[i + j] == needle[j])
+		j++;
+		if (!needle[j])
+		return (&haystack[i]);
+	}
+	return (0);
 }
