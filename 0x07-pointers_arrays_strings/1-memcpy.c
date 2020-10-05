82c63a2f99ad08794bb70c0ce40f7163f126a81d
aminbnd <2212@holbertonschool.com>
2020-10-05 14:40:42 +0200

    memcpy update style

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -8,14 +8,15 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-unsigned int i, j;
-for (i = 0; i < n; i++)
-{
-dest[i] = src[j];
-for (j = 0; j < n; j++)
-{
-dest[j] = src[j];
-}
-}
-return (dest);
+	unsigned int i, j;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[j];
+		for (j = 0; j < n; j++)
+		{
+			dest[j] = src[j];
+		}
+	}
+	return (dest);
 }
