614f7f1057fff4b35e8df6c6f2f6cd222e130803
aminbnd <2212@holbertonschool.com>
2020-10-05 20:30:53 +0200

    strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,26 +1,21 @@
 #include "holberton.h"
 /**
- * *_strstr - locates a substring
+ * _strstr -  locates a substring
  * @haystack: pointer to the string to be "scanned"
- * @needle: substring to be found
- * Return: pointer to the first occurence of the in the haystack string
- * of any of entire sequence specified in needle
+ * @needle: substring to be found 
+ * Return: pointer
  */
 char *_strstr(char *haystack, char *needle)
 {
-	int i = 0;
-	int j;
+        int i, j;
 
-	for (i = 0; haystack[i] != '\0'; i++)
-	{
-		for (j = 0; needle[j] != '\0'; j++)
-		{
-			if (needle[j + i] && haystack[i] == needle[j])
-			{
-				return (&haystack[i]);
-			}
-		}
-	}
-	return (0);
+        for (i = 0; haystack[i]; i++)
+        {
+                j = 0;
+                while (needle[j] && haystack[i + j] == needle[j])
+                j++;
+                if (!needle[j])
+                return (&haystack[i]);
+        }
+        return (0);
 }
-
