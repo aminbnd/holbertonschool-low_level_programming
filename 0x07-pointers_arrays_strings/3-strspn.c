406275a48ed413cbc38c2309fa3562b41a15b893
aminbnd <2212@holbertonschool.com>
2020-10-05 12:21:10 +0200

    strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: pointer
+ * @accept: pointer
+ * Return: integer
+ */
+ unsigned int _strspn(char *s, char *accept)
+ {
+        int n;
+
+
+
+
+
+        return (n);
+ }
