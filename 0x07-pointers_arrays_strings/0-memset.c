0d2b6e17bc162846701291508fb8583b4a6b61b7
aminbnd <2212@holbertonschool.com>
2020-10-05 07:48:53 +0200

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,18 +2,20 @@
 /**
  * *_memset - fills memory with a constant byte
  * @s: pointer
+ * @b: character
+ * @n: unsigned integer
  * Return: pointer to the memory area s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-    unsigned int i, j;
-    for (i = 0; i < n; i++)
-    {
-        s[i] = b;
-        for (j = 0; j < n; j++)
-        {
-            s[j] = b;
-        }
-    }
-    return (s);
+unsigned int i, j;
+for (i = 0; i < n; i++)
+{
+s[i] = b;
+for (j = 0; j < n; j++)
+{
+s[j] = b;
+}
+}
+return (s);
 }
