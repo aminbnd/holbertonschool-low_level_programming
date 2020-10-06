d5e272d695794922d1b39dc5df1b0a53f0161db0
aminbnd <2212@holbertonschool.com>
2020-10-07 00:27:19 +0200

    98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,17 +9,17 @@ void print_to_98(int n)
 {
 	int i;
 
-	if (n < 98)
-	{
-		for (i = n; i < 98; i++)
-			printf("%d, ", i);
-		printf("98 \n");
-	}
-	if (n == 98)
-		printf("%d\n", i);
 	if (n > 98)
 	{
 		for (i = n; i > 98; i--)
+			printf("%d, ", i);
+		printf("98\n");
+	}
+	if (n == 98)
+	printf("%d\n", n);
+	if (n < 98)
+	{
+		for (i = n; i < 98; i++)
 			printf("%d, ", i);
 		printf("98\n");
 	}
