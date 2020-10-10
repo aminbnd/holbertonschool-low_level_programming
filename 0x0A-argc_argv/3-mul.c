24d1ff37dc5a7540efc62281bd18bc5cc3f52dc5
aminbnd <2212@holbertonschool.com>
2020-10-10 12:01:43 +0200

    mult arguments

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - multiplies two numbers
+ * @argc: integer, number of arguments
+ * @argv: vector, array of string
+ * Return: 0 (success)
+ */
+int main(int argc, char **argv)
+{
+	if (argc < 3)
+	{
+		puts("ERROR");
+		return (1);
+	}
+	printf("%d\n", atoi(argv[2]) * atoi(argv[1]));
+	return (0);
+}
