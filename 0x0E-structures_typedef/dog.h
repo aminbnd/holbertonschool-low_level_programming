e6cfd9f4da7ee2f124d91e4f2fae06b98b0e5ae1
aminbnd <2212@holbertonschool.com>
2020-10-19 20:43:07 +0200

    structure

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - information about a dog
+ *@name: pointer to a string
+ *@age: float
+ *@owner: pointer to a string
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
