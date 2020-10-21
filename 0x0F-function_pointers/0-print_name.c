69f958020bfc2b260d4c1b8615b55e0b3cda1cf2
aminbnd <2212@holbertonschool.com>
2020-10-21 22:50:49 +0200

    print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+/**
+ * print_name - Prints a name
+ * @name: name to print
+ * @f: function to call to print
+ * Return: Nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,10 @@
+#ifndef HEADER_File
+#define HEADER_FILE
+void print_name(char *name, void (*f)(char *));
+
+
+
+
+
+
+#endif
