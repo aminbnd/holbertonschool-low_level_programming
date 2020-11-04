18b9209e0433b7d531470bfaad058adf4d25da4a
aminbnd <2212@holbertonschool.com>
2020-11-04 19:32:06 +0100

    get node at index

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -15,7 +15,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 		head = head->next;
 		count++;
 	}
-	if (count < index)
+	if (count != index)
 		return (NULL);
 	return (head);
 }
