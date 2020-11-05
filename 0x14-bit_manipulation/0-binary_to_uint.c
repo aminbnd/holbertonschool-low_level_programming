f22feae51a1f5e9016b0695b5da632ceeebf0229
aminbnd <2212@holbertonschool.com>
2020-11-05 21:35:04 +0100

    binary to insigned int

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b: string that containes binary
+ * Return: unsigned int (converted number)
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int dec = 0;
+	int count = 0;
+
+	if (!b)
+		return (0);
+	while (b[count])
+	{
+		if (b[count] != '0' && b[count] != '1')
+			return (0);
+		count++;
+	}
+	count = 0;
+	while (b[count])
+	{
+		dec <<= 1;
+		dec += b[count] - '0';
+		count++;
+	}
+	return (dec);
+}
